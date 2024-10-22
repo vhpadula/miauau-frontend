1796adc3b2899a6a91bdcacfb0eb4f11eb8f8ac4
leticiacohen <leticiacohenferrari@gmail.com>
2024-10-22 00:01:56 -0300

    create animal page

---
+++ src/app/(animalOrg)/animals/animal/page.tsx
@@ -0,0 +1,136 @@
+"use client";
+import React from "react";
+import AnimalCard from "@/components/molecules/AnimalCard";
+import Image from "next/image";
+import { AdoptionCandidateCard, Button } from "@/components";
+
+
+export default function Animal () {
+    const candidates = [
+        {
+            id: "1",
+            name: "Rex",
+            age: "2 anos",
+            livesAlone: false,
+            occupation: "Trabalha"
+        },
+        {
+            id: "2",
+            name: "Mimi",
+            age: "3 anos",
+            livesAlone: false,
+            occupation: "Trabalha"
+        },
+        {
+            id: "3",
+            name: "Buddy",
+            age: "4 anos",
+            livesAlone: false,
+            occupation: "Trabalha"
+        },
+        {
+            id: "4",
+            name: "Luna",
+            age: "1 ano",
+            livesAlone: false,
+            occupation: "Trabalha"
+        },
+        {
+            id: "5",
+            name: "Max",
+            age: "5 anos",
+            livesAlone: false,
+            occupation: "Trabalha"
+        },
+        {
+            id: "6",
+            name: "Bella",
+            age: "2 anos",
+            livesAlone: false,
+            occupation: "Trabalha"
+        },
+        {
+            id: "7",
+            name: "Charlie",
+            age: "3 anos",
+            livesAlone: false,
+            occupation: "Trabalha"
+        },
+        {
+            id: "8",
+            name: "Simba",
+            age: "4 anos",
+            livesAlone: false,
+            occupation: "Trabalha"
+        },
+    ];
+    const animal = {
+        id: "2",
+        imageSrc: "/images/cat1.png",
+        name: "Mimi",
+        species: "Felino",
+        size: "Médio",
+        age: "3 anos",
+        location: "Rio de Janeiro",
+    };
+
+    return (
+        <>
+            <div className="flex flex-col items-center pt-20">
+                <div className="bg-secondary w-full flex flex-row items-center px-5 pt-2 pb-20">
+                    <Image src={animal.imageSrc} alt="Animais" width={100} height={100} className="border-[3px] border-white rounded-full my-6"/>
+                    <div className="ml-3 flex-grow">
+                        <p className="font-bold text-white text-xl">{animal.name}</p>
+                        <p className="text-white text-xs font-light">{animal.species.toUpperCase()} | PORTE {animal.size.toUpperCase()} | {animal.age.toUpperCase()}</p>
+                        <p className="text-white text-xs font-light">{animal.location}</p>
+                        <Button
+                            label="Levar para a feira de adoção"
+                            className="text-xs w-full mt-2"
+                            variant="primary"
+                            type="submit"
+                        />
+                    </div>
+                </div>
+                <div className="px-4 w-full -m-3">
+                    <div className="flex items-start justify-between bg-white shadow-md rounded-lg -mt-16">
+                        <div className="flex items-center justify-between">
+                            <div className="flex flex-col items-center justify-center m-5">
+                                <Image src="/icons/cat-dog-vet.svg" alt="Animais" width={50} height={50}/>
+                                <p className="text-secondary font-extrabold text-xs mt-2">SITUAÇÃO</p>
+                                <p className="text-secondary font-extrabold text-xs">CLÍNICA</p>
+                            </div>
+                            <div className="grid grid-cols-2 gap-x-2 gap-y-0.5 text-gray-700 text-xs font-medium flex-grow">
+                                <p>Vacinas:</p>
+                                <p>3 de 4</p>
+
+                                <p>FIV/FELV:</p>
+                                <p>negativo</p>
+
+                                <p>Castrado:</p>
+                                <p>sim</p>
+
+                                <p>Vermifugado:</p>
+                                <p>sim</p>
+                            </div>
+                        </div>
+                    <Image src="/icons/edit-primary.svg" alt="Animais" width={20} height={20} className="m-5"/>
+                    </div>
+                </div>
+            </div>
+                <p className="text-secondary font-bold text-xl mt-12 mx-5 pb-5">Candidatos à adoção</p>
+                <div className="grid grid-cols-1 lg:w-2/3 md:w-1/2 gap-4 mx-5  max-h-screen">
+                    {candidates.map((candidate) => (
+                        <AdoptionCandidateCard
+                            key={candidate.id}
+                            id={candidate.id}
+                            name={candidate.name}
+                            occupation={candidate.occupation}
+                            livesAlone={candidate.livesAlone}
+                            age={candidate.age}
+                        />
+                    ))}
+                </div>
+        </>
+    );
+};
+
--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -2,3 +2,4 @@ export { default as TitleLogo } from "./TitleLogo";
 export { default as HeaderGroup } from "./HeaderGroup";
 export { default as AnimalCard } from "./AnimalCard";
 export { default as VolunteerCard } from "./VolunteerCard";
+export { default as AdoptionCandidateCard } from "./AdoptionCandidateCard"
