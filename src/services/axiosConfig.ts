35cca396ab906f762f75609ef40a7a654d115fd1
Michel Brito <michel.brito@inspira.com.br>
2024-11-15 22:14:42 -0300

    add header

--- src/services/axiosConfig.ts
+++ src/services/axiosConfig.ts
@@ -4,6 +4,7 @@ const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     headers: {
         "Content-Type": "application/json",
+        "ngrok-skip-browser-warning": '1'
     },
 });
 
