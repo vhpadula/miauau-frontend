8c5aa31297f1d819d167cbef7043ba983269a2f2
vhpadula <victorhpadula@gmail.com>
2024-12-03 01:42:14 -0300

    made changes to about page. added more text and images

--- src/constants/about.tsx
+++ src/constants/about.tsx
@@ -1,5 +1,8 @@
 export const aboutInfo = {
-    title: "O presente sistema foi desenvolvido por estudantes da Escola Politécnica da USP, Letícia, Michel e Victor.",
-    description: "O objetivo é auxiliar ONGs na gestão de seus recursos e animais, agilizando o processo de adoção.",
-    stack: "O sistema foi desenvolvido utilizando Next.js, TailwindCSS, TypeScript para o frontend, já o backend foi desenvolvido em Java, com o framework Spring. A arquitetura do sistema é baseada em microsserviços, de forma a tornar o sistema modular e escalável, podendo se adaptar ao contexto de outras ONGs.",
+  title:
+    "O presente sistema foi desenvolvido por estudantes da Escola Politécnica da USP, Letícia Cohen, Michel Brito e Victor Padula.",
+  description:
+    "O objetivo é auxiliar ONGs na gestão de seus recursos. A primeira implementação é no contexto de resgate e adoção de animais, agilizando o processo de adoção.",
+  stack:
+    "O sistema foi desenvolvido utilizando Next.js, TailwindCSS, TypeScript para o frontend, já o backend foi desenvolvido em Java, com o framework Spring. A arquitetura do sistema é baseada em microsserviços, de forma a tornar o sistema modular e escalável, podendo se adaptar ao contexto de diferentes ONGs e ganhar features adicionais com facilidade.",
 };
